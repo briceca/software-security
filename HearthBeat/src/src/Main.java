1af781cff82f4f99de85148d774aefeced349816
briceca <brice.cammermans@student.ehb.be>
2017-05-22 15:11:21 +0200

    durable queue

---
+++ HearthBeat/src/src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * Created by brice on 22/05/2017.
+ */
+import com.rabbitmq.client.*;
+
+
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import org.json.*;
+//import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.*;
+
+public static void main(String[] args) {
+
+        }
